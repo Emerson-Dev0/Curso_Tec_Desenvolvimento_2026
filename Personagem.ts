9c1bee156c72a404af819f820078174062d7dd52
Emerson <emersonrodrigues0428@gmail.com>
2026-04-09 19:35:57 -0300

    Sisema de regen de vida, Download de NPX

--- Personagem.ts
+++ Personagem.ts
@@ -1,21 +1,40 @@
 export abstract class Personagem {
-   public nome: string = "Personagem";
-   protected poder_de_ataque: number = 0;
-   protected vida: number = 0;
-     
-     constructor(nome:string, poder_de_ataque:number, vida:number){
-      this.nome = nome;
-      this.poder_de_ataque = poder_de_ataque;
-      this.vida = vida;
-     }
-     isContinuaVivo(): boolean{
-      return this.vida > 0
-     }
-     sofreu_dano(dano:number): void{
-      this.vida = this.vida - dano;
-      console.log(`${this.nome} recebeu ${dano} de dano. vida atual: ${this.vida}`)
-     }
-     abstract atacar(persona:Personagem):void;
+  public nome: string = "Personagem";
+  protected poder_de_ataque: number = 0;
+  protected vida: number = 0;
+  protected vidaMaxima: number;
 
-     
+  constructor(nome: string, poder_de_ataque: number, vida: number){
+    this.nome = nome;
+    this.poder_de_ataque = poder_de_ataque;
+    this.vida = vida;
+    this.vidaMaxima = vida;
+  }
+
+  isContinuaVivo(): boolean {
+    return this.vida > 0;
+  }
+
+  sofreu_dano(dano: number): void {
+    this.vida = Math.max(0, this.vida - dano);
+    console.log(`${this.nome} recebeu ${dano} de dano. vida atual: ${this.vida}`);
+  }
+
+  gerarAtaque(): number {
+    return Math.floor(Math.random() * 3);
+  }
+
+  curarSeNecessario(): void {
+    if (this.vida < this.vidaMaxima * 0.3) {
+      this.vida += this.vidaMaxima * 0.2;
+
+      if (this.vida > this.vidaMaxima) {
+        this.vida = this.vidaMaxima;
+      }
+
+      console.log(`${this.nome} regenerou vida! Vida atual: ${this.vida}`);
+    }
+  }
+
+  public abstract atacar(persona: Personagem): void;
 }
--- mago.ts
+++ mago.ts
@@ -1,12 +1,28 @@
 import { Personagem } from "./Personagem.ts";
 
-export class Mago extends Personagem{
-     constructor(nome:string, poder_de_ataque:number, vida:number){
-        super(nome,poder_de_ataque,vida);
-     }
+export class Mago extends Personagem {
+  constructor(nome: string, poder_de_ataque: number, vida: number) {
+    super(nome, poder_de_ataque, vida);
+  }
 
-     atacar(persona: Personagem): void {
-         console.log(`${this.nome} Chuta que é macumba`);
-         persona.sofreu_dano(this.poder_de_ataque);
-     }
+  atacar(persona: Personagem): void {
+    const tipoAtaque = this.gerarAtaque();
+
+    let dano = 0;
+
+    if (tipoAtaque === 0) {
+      dano = this.poder_de_ataque;
+      console.log(`${this.nome} usou baratas mexicanas 🐜`);
+    } 
+    else if (tipoAtaque === 1) {
+      dano = this.poder_de_ataque * 1.5;
+      console.log(`${this.nome} usou 71kg de batata doce 🍠`);
+    } 
+    else {
+      dano = this.poder_de_ataque * 2;
+      console.log(`${this.nome} usou Feijão com farinha 💀`);
     }
+
+    persona.sofreu_dano(dano);
+  }
+}
